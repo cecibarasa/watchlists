c2350478d183ad0cfac35d423da22b2f465a9704
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-04 15:07:53 +0300

    modify review field

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,9 +4,11 @@ from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
-    submit = SubmitField('Submit')
+ title = StringField('Review title',validators=[Required()])
+
+ review = TextAreaField('Movie review')
+
+ submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
